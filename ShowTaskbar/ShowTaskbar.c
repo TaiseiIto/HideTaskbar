562cade8c362fea26a7d77f69344ee3ee39077ee
TaiseiIto <taisei1212@outlook.jp>
2022-08-13 11:41:14 +0900

    deploy source

---
+++ ShowTaskbar/ShowTaskbar.c
@@ -0,0 +1,13 @@
+#include<windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreviousInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+	HWND hShell_TrayWnd = NULL;
+	HWND hShell_SecondaryTrayWnd = NULL;
+	LPSTR lpszShell_TrayWndClassName = "Shell_TrayWnd";
+	LPSTR lpszShell_SecondaryTrayWndClassName = "Shell_SecondaryTrayWnd";
+	while((hShell_TrayWnd = FindWindowEx(GetDesktopWindow(), hShell_TrayWnd, lpszShell_TrayWndClassName, NULL)) != NULL)ShowWindow(hShell_TrayWnd, SW_RESTORE);
+	while((hShell_SecondaryTrayWnd = FindWindowEx(GetDesktopWindow(), hShell_SecondaryTrayWnd, lpszShell_SecondaryTrayWndClassName, NULL)) != NULL)ShowWindow(hShell_SecondaryTrayWnd, SW_RESTORE);
+	return EXIT_SUCCESS;
+}
+
